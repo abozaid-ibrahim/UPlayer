125a9fb9140810075ef87a58e958da96eb3d901d
abuzeid <abuzeid7@gmail.com>
2020-11-28 21:38:55 +0100

    Add ui-test case to happy scenario navigation.

--- MimiMusicPlayerUITests/MimiMusicPlayerUITests.swift
+++ MimiMusicPlayerUITests/MimiMusicPlayerUITests.swift
@@ -8,36 +8,33 @@
 
 import XCTest
 
-class MimiMusicPlayerUITests: XCTestCase {
-
+final class MimiMusicPlayerUITests: XCTestCase {
     override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-
-        // In UI tests it is usually best to stop immediately when a failure occurs.
         continueAfterFailure = false
-
-        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
     }
 
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() throws {
-        // UI tests must launch the application that they test.
+    func testNavigationBetweenAppScreensForTheHappyPathScenario() throws {
         let app = XCUIApplication()
+        app.launchArguments.append("--uitesting")
         app.launch()
-
-        // Use recording to get started writing UI tests.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    func testLaunchPerformance() throws {
-        if #available(macOS 10.15, iOS 13.0, tvOS 13.0, *) {
-            // This measures how long it takes to launch your application.
-            measure(metrics: [XCTOSSignpostMetric.applicationLaunch]) {
-                XCUIApplication().launch()
-            }
+        func assertArtistsListExistsThenSelectFirstArtist() {
+            let artistsTable = app.tables["ArtistsTable"]
+            XCTAssertTrue(artistsTable.exists)
+            artistsTable.cells.firstMatch.tap()
         }
+        func assertSongsListExistsThenPlayFirstSong() {
+            XCTAssertTrue(app.tables["SongsTable"].exists)
+            app.tables["SongsTable"].cells.firstMatch.tap()
+        }
+        func assertPlayerViewExistAfterNavigationBack() {
+            XCTAssertTrue(app.otherElements["PlayerView"].waitForExistence(timeout: 0.01))
+            XCTAssertTrue(app.buttons["play"].exists)
+            app.buttons["play"].tap()
+            app.navigationBars.buttons.element(boundBy: 0).tap()
+            XCTAssertTrue(app.otherElements["PlayerView"].exists)
+        }
+        assertArtistsListExistsThenSelectFirstArtist()
+        assertSongsListExistsThenPlayFirstSong()
+        assertPlayerViewExistAfterNavigationBack()
     }
 }
